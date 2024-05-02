aef88a9c80dcfafd580e26e72798dcd1b1073150
pufferbommy <pufferbommy@gmail.com>
2024-05-02 09:16:33 +0700

    fix: can click when show loader

--- src/components/full-loader.tsx
+++ src/components/full-loader.tsx
@@ -1,5 +1,5 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { useStore } from "@nanostores/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { $isLoading } from "@/store/full-loader";
 
@@ -12,17 +12,14 @@ const FullLoader = () => {
         <motion.div
           initial={{
             opacity: 0,
-            pointerEvents: "none",
           }}
           animate={{
             opacity: 1,
-            pointerEvents: "auto",
           }}
           exit={{
             opacity: 0,
-            pointerEvents: "none",
           }}
-          className="fixed inset-0 bg-black/25 z-[999999] flex justify-center items-center before:w-16 before:h-16 before:animate-spin before:border-8 before:rounded-full before:border-white before:border-t-primary"
+          className="pointer-events-none fixed inset-0 bg-black/25 z-[999999] flex justify-center items-center before:w-16 before:h-16 before:animate-spin before:border-8 before:rounded-full before:border-white before:border-t-primary"
         />
       )}
     </AnimatePresence>
