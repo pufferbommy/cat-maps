109f12d15cfac67dadbbc94d5e80dcdf52159511
pufferbommy <pufferbommy@gmail.com>
2024-07-15 23:03:18 +0700

    chore: change skeleton shape of loading profile

--- apps/client/src/components/layout/header/index.tsx
+++ apps/client/src/components/layout/header/index.tsx
@@ -19,31 +19,33 @@ const Navbar = () => {
           Cat Maps
         </Link>
         {!isLoading ? (
-          <div className="flex gap-4 items-center">
-            {!userProfile ? (
-              <div className="flex gap-2">
-                <AuthButton variant="outline" initialAction="login" />
-                <AuthButton initialAction="register" />
-              </div>
-            ) : (
-              <>
-                <div className="flex items-center gap-2">
-                  <Avatar
-                    variant="beam"
-                    size={32}
-                    name={userProfile.username}
-                  />
-                  <span className="capitalize">{userProfile.username}</span>
+          <>
+            <div className="flex gap-4 items-center">
+              {!userProfile ? (
+                <div className="flex gap-2">
+                  <AuthButton variant="outline" initialAction="login" />
+                  <AuthButton initialAction="register" />
                 </div>
-                <LogoutButton />
-              </>
-            )}
-          </div>
+              ) : (
+                <>
+                  <div className="flex items-center gap-2">
+                    <Avatar
+                      variant="beam"
+                      size={32}
+                      name={userProfile.username}
+                    />
+                    <span className="capitalize">{userProfile.username}</span>
+                  </div>
+                  <LogoutButton />
+                </>
+              )}
+            </div>
+          </>
         ) : (
-          <div className="flex gap-2 items-center">
-            <Skeleton className="w-7 h-7 rounded-full" />
-            <Skeleton className="w-10 h-3 rounded-full" />
-            <Skeleton className="w-3 h-3 rounded-full" />
+          <div className="flex gap-2 items-center *:rounded-full">
+            <Skeleton className="w-8 h-8" />
+            <Skeleton className="w-12 h-4" />
+            <Skeleton className="size-4" />
           </div>
         )}
       </div>
