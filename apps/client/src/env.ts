28f097454b1f160be1e5ddcf876d643b06ba69b7
pufferbommy <pufferbommy@gmail.com>
2024-07-22 11:02:37 +0700

    refactor: clear code

--- apps/client/src/env.ts
+++ apps/client/src/env.ts
@@ -5,17 +5,7 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SERVER_URL: z.string(),
   },
-  server: {
-    NODE_ENV: z.enum(["development", "test", "production"]),
-    CLOUDINARY_CLOUD_NAME: z.string(),
-    CLOUDINARY_API_KEY: z.string(),
-    CLOUDINARY_API_SECRET: z.string(),
-  },
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   },
 });
